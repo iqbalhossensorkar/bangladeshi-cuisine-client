408dd7bb84da9a2cf637e8d16437d4fa4caef62a
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-05-02 03:06:58 +0600

    Firebase environment added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDsrd78RTa5yxmeYQCQOicT2GYY80O511U",
-  authDomain: "bangladeshi-cuisine.firebaseapp.com",
-  projectId: "bangladeshi-cuisine",
-  storageBucket: "bangladeshi-cuisine.appspot.com",
-  messagingSenderId: "812793741857",
-  appId: "1:812793741857:web:9e18eaee8999f0ab41d960"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
